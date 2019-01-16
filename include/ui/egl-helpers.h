4b7170dda94d0882d62f3dd3adb57f2a8f19db4e
Alexander Kanavin <alex.kanavin@gmail.com>
2019-01-16 12:37:51 +0100

    fbd57c754f32804a63295f70f271d1ef128ee590
    egl-helpers.h: do not depend on X11 Window type, use EGLNativeWindowType
    
    It was assumed that mesa provides the necessary X11 includes,
    but it is not always the case, as it can be configured without x11 support.
    
    Signed-off-by: Alexander Kanavin <alex.kanavin@gmail.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-id: 20190116113751.17177-1-alex.kanavin@gmail.com
    
    [ kraxel: codestyle fix (long line) ]
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- include/ui/egl-helpers.h
+++ include/ui/egl-helpers.h
@@ -42,7 +42,7 @@ void egl_dmabuf_release_texture(QemuDmaBuf *dmabuf);
 
 #endif
 
-EGLSurface qemu_egl_init_surface_x11(EGLContext ectx, Window win);
+EGLSurface qemu_egl_init_surface_x11(EGLContext ectx, EGLNativeWindowType win);
 
 int qemu_egl_init_dpy_x11(EGLNativeDisplayType dpy);
 int qemu_egl_init_dpy_mesa(EGLNativeDisplayType dpy);
